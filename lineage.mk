a3b46f0e01d1fc989e397932c13e56c66c3226e0
virsys <virsys.electron@gmail.com>
2020-03-25 15:01:21 +0100

    Remove device identifiers They have to be in the specific repository anyways.

--- lineage.mk
+++ lineage.mk
@@ -7,9 +7,3 @@ $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_CHARACTERISTICS := tablet
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
